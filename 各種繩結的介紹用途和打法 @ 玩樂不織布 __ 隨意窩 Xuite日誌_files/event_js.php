04585f91c7a716a2bfdd28c08ad8457f2e755dd0
jash-git <jash.liao@gmail.com>
2019-01-29 11:56:44 +0800

    Create

---
+++ 各種繩結的介紹用途和打法 @ 玩樂不織布 __ 隨意窩 Xuite日誌_files/event_js.php
@@ -0,0 +1,43 @@
+    /* 
+      title:標題
+      link:連結
+      thumb:縮圖(可以放在一篇文章中)
+      description:說明(前端會抓40字元內)
+      date:起迄日
+    */
+var count=0, angel = new Array();count++;angel[count] = {
+				title:'梅花湖農場體驗下殺75折',
+				link:'https://www.umaji.com.tw/#/info4',
+				thumb:'https://8.share.photo.xuite.net/event/18c54ca/19526729/1204065665_o.jpg',
+				description:'梅花湖農場體驗下殺75折，$279起送免費下午茶',
+				date:'2019/01/22~2019/02/28'
+			
+			};count++;angel[count] = {
+				title:'春節期間國五客運獨享',
+				link:'https://www.umaji.com.tw/#/info4',
+				thumb:'https://8.share.photo.xuite.net/event/18c54ad/19526729/1204060772_o.jpg',
+				description:'春節連假期間國五客運獨享72折',
+				date:'2019/01/22~2019/02/10'
+			
+			};count++;angel[count] = {
+				title:'車票72折加碼送',
+				link:'https://www.umaji.com.tw/#/info4',
+				thumb:' https://8.share.photo.xuite.net/event/18c540e/19526729/1204064965_o.jpg',
+				description:'限時預購！車票72折加碼送點數50元',
+				date:'2019/01/22~2019/01/31'
+			
+			};count++;angel[count] = {
+				title:'動漫大小事報你知',
+				link:'https://goo.gl/QKotuy',
+				thumb:'https://8.share.photo.xuite.net/event/18c541d/19526729/1203237588_o.jpg',
+				description:'Cosplay與同人誌都屬於次文化，但你知道它們的差別嗎？',
+				date:'2019/01/10~2019/02/11'
+			
+			};count++;angel[count] = {
+				title:'年節9連休出遊看這邊',
+				link:'https://hamipoint.cht.com.tw/upload/cheers_event/hamipoint_activity4.php',
+				thumb:'https://8.share.photo.xuite.net/event/18c5455/19526729/1201815308_o.jpg',
+				description:'買鳳凰旅行社團體行程透過Hami Point回饋最高600點，日本團體賞雪行程也有回饋!!',
+				date:'2018/12/19~2019/12/31'
+			
+			};
